2bbeed0031e1fdd02c6d180e6537e5985448549b
Declan Simkins <simkinsd@mymacewan.ca>
2019-03-20 15:54:30 -0600

    Provide code to connect to database
    There are a few variables inside the main function that can be
    changed to specify the attributes of the server you are trying to
    connect to.
    
    The ones that are present are for connecting to the local server on my
    laptop; these will have to be changed if you want to connect to the
    server setup on your own computer. This will always be the case unless
    we were able to set up a remote server (unlikely and probably not worth
    the trouble)

---
+++ ConnectionTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Text;
+
+namespace ConnectTest
+{
+	class SQLConnect
+	{
+		static void Main(string[] args)
+		{
+			string connString;
+			SqlConnection cnn;
+
+			connString = @"Data Source=SURFACE-LAPTOP-\MSSQLSERVER01;Initial Catalog=291Project;User Id=admin;Password=12345";
+			cnn = new SqlConnection(connString);
+			cnn.Open();
+			cnn.Close();
+		}
+	}
+}
