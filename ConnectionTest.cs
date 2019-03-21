190a1e89882c29b77ce2262da2f42281f61f5800
Declan Simkins <simkinsd@mymacewan.ca>
2019-03-20 20:10:49 -0600

    Package DB connecting and querying into a class
    Simplifies connecting to the database and running a query
    a little bit and provides a good basis if we need to make
    modifications to how we run queries or connect to the database
    later by abstracting the details into a little class

--- ConnectionTest.cs
+++ ConnectionTest.cs
@@ -2,19 +2,51 @@
 using System.ComponentModel;
 using System.Text;
 
-namespace ConnectTest
+namespace CMPT291_Project
 {
-	class SQLConnect
+	class ConnectTest
 	{
 		static void Main(string[] args)
 		{
-			string connString;
-			SqlConnection cnn;
+			string dataSource = @"SURFACE-LAPTOP-\MSSQLSERVER01";
+			string dbName = "291Project";
+			string username = "admin";
+			string password = "12345";
 
-			connString = @"Data Source=SURFACE-LAPTOP-\MSSQLSERVER01;Initial Catalog=291Project;User Id=admin;Password=12345";
-			cnn = new SqlConnection(connString);
-			cnn.Open();
-			cnn.Close();
+			DBManager db = new DBManager(dataSource, dbName, username, password);
+			SqlDataReader dr = db.query("SELECT FirstName FROM Patient");
+
+			while (dr.Read())
+			{
+				System.Console.WriteLine(dr.GetValue(0));
+			}
+
+			db.closeConnection();
+
+			//string connString;
+			//SqlConnection cnn;
+
+			//connString = @"Data Source=" + dataSource + ";Initial Catalog=" + dbName
+			//	+ ";User Id=" + username + ";Password=" + password;
+			//cnn = new SqlConnection(connString);
+			//cnn.Open();
+
+
+			//SqlCommand comm;
+			//SqlDataReader dataReader;
+			//string sql;
+
+			//sql = "SELECT FirstName FROM Patient";
+			//comm = new SqlCommand(sql, cnn);
+			//dataReader = comm.ExecuteReader();
+
+			//while (dataReader.Read())
+			//{
+			//	System.Console.WriteLine(dataReader.GetValue(0));
+			//}
+
+
+			//cnn.Close();
 		}
 	}
 }
